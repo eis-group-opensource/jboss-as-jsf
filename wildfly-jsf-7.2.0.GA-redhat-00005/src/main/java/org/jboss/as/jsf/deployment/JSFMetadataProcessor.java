e856ea05c1b569647fa5d32d35de58a5a9951098
eis-group-opensource <eis-group-opensource@eisgroup.com>
2021-11-18 16:29:44 +0200

    Repository sync on 18-11-2021_16:29

---
+++ wildfly-jsf-7.2.0.GA-redhat-00005/src/main/java/org/jboss/as/jsf/deployment/JSFMetadataProcessor.java
@@ -0,0 +1,21 @@
+package org.jboss.as.jsf.deployment;
+
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+
+public class JSFMetadataProcessor implements DeploymentUnitProcessor {
+
+    public JSFMetadataProcessor(Boolean value) {
+
+    }
+
+    @Override
+    public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+    }
+}
